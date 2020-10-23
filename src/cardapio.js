5b2b55e7afb2ff07b3220e612541983cb6d0ff00
Ednilton Junior <jrjunior_cg@live.com>
2020-10-22 21:09:16 -0300

    Inicialized Repository

---
+++ src/cardapio.js
@@ -0,0 +1,16 @@
+const cardapio = {
+    1: {
+        descricao: "X-Burguer",
+        preco: 9.90
+    },
+    2: {
+        descricao: "Hamburguer",
+        preco: 5.00
+    },
+    3: {
+        descricao: "Coxinha",
+        preco: 2.00
+    }
+}
+
+exports.menu = cardapio;
